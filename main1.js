22631eae011deb77d4e7ba5f4d6799c67351c622
mariagres07 <mariagresia347@gmail.com>
2025-03-24 23:40:53 +0700

    coba bikin pesan

--- main1.js
+++ main1.js
@@ -25,7 +25,6 @@ function hasilInputPilihan(){
     }
     hasilInput += '<button onclick="teksPilihan()">OK</button>';
     masukkanTeks.innerHTML = hasilInput;
-
 }
 
 function teksPilihan(){
@@ -33,10 +32,29 @@ function teksPilihan(){
     const pilihTeks = document.getElementById('pilihTeks');
 
     let hasilDipilih = '<h4>Teks Pilihan:</h4>';
+
+    
     for (let i = 1; i <= totalPilihan; i++) {
-        let pilihanValue = document.getElementById(`pilihan${i}`).value;
-        hasilDipilih += `<p>Pilihan ${i}: ${pilihanValue}</p>`;
+        let pilihan = document.getElementById(`pilihan${i}`).value;
+        hasilDipilih += `<p>Pilihan ${i}: ${pilihan}</p>`;
     }
 
     pilihTeks.innerHTML = hasilDipilih;
+}
+
+function pesan(){
+    const name = document.getElementById('nama').value;
+    const totalPilihan =  parseInt(document.getElementById('jumlahPilihan').value);
+    const pesan = document.getElementById('pesan');
+
+    let pilihanTeks = [];
+
+    for (let i = 1; i <= totalPilihan; i++) {
+        let pilihan = document.getElementById(`pilihan ${i}`).value;
+        pilihanTeks.push(pilihan);
+    }
+
+    let pesanHalo = `Hallo, nama saya ${name}, saya mempunyai sejumlah ${totalPilihan} pilihan, yaitu ${pilihanTeks.join(', ')}, dan saya memilih ${pilihanTerpilih}.`;
+    
+    alert(pesanHalo);
 }
